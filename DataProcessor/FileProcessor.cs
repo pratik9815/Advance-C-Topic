57f53b9301f9f8982874ff9e3a48ba19a42f2962
pratik9815 <pchamlagain12@gmail.com>
2024-08-29 20:57:40 +0545

    first commit

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor;
+
+public class FileProcessor
+{
+    private const string BackupDirectoryName = "backup";
+    public string? InputFilePath { get; }
+    public FileProcessor(string? inputFilePath)
+    {
+        InputFilePath = inputFilePath;
+    }
+    public void Process() 
+    {
+        Console.WriteLine($"Begin the process of {InputFilePath}");
+
+        //check if the file exists
+        if(!File.Exists(InputFilePath))
+        {
+            Console.WriteLine($"Error: file {InputFilePath} doesnot exists.");
+            return;
+        }
+        //getting the root directory path
+        string? rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent?.FullName;
+
+        if(!Directory.Exists(rootDirectoryPath))
+        {
+
+        }
+    }
+}
---
+++ DataProcessor/JsonSerialization.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+namespace DataProcessor
+{
+    public class JsonSerialization
+    {
+
+        //This is by using system.text.json we can do another example using netwonsoft.json
+        public void Serializer()
+        {
+            string jsonString = @"
+                                {
+                                    ""success"": true,
+                                    ""message"": ""This is a message from our json data"",
+                                    ""data"": [{
+                                            ""name"":""Pratik""
+                                        }]
+                                }";
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true, //This will properly format the output of data
+                PropertyNameCaseInsensitive = true, // the output will be mapped to the related calss if there is one irrespective to the word casing
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, // this will ignore the characters that will affect the 
+            };
+
+            //While we deserialize we convert this json object into an object
+            object? json = JsonSerializer.Deserialize<object>(jsonString);
+            //when we serialize we convert the deserialized object into an string
+            string jsonStr = JsonSerializer.Serialize(json);
+
+            //we can access the member using jsondocument with the jsonelement
+
+            JsonDocument doc = JsonDocument.Parse(jsonStr); // first we parse to get the json from the string value
+            JsonElement element = doc.RootElement;  //we can get the root element by using rootelement to jsondocument and get a jsonelement type
+            string messsage = element.GetProperty("message").ToString(); // we can extract message using get property method
+
+            //to get the array first we should be sure that an array will exist in the json string
+
+            var array = element.GetProperty("data"); // this will return an exception if the data does not exist.
+
+            //To check we can do jsonelement.valuekind which can check if it is an array type, string type , object type or other
+            if(array.ValueKind == JsonValueKind.Array)
+            {
+                Console.WriteLine("This is a type of array");
+            }
+            
+            //to be super safe we can do this while checking for array 
+            if(element.TryGetProperty("data", out JsonElement dataElement) && dataElement.ValueKind == JsonValueKind.Array )
+            {
+                Console.WriteLine("This is an array");
+                if (dataElement.GetArrayLength() > 0)
+                {
+                    Console.WriteLine($"This is an array of length {dataElement.GetArrayLength()}");
+                }
+                else
+                {
+                    Console.WriteLine("This is an empty array");
+                }
+            }
+        }
+    }
+}
